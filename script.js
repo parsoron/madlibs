b48ee7fc2ec4b189a8c1b128ee429ea0f5c7776f
parsoron <53783158+parsoron@users.noreply.github.com>
2019-11-15 09:57:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function bruh(){
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  let c = document.getElementById("c").value;
+  let d = document.getElementById("d").value;
+  let e = document.getElementById("e").value;
+  let f = document.getElementById("f").value;
+  let g = document.getElementById("g").value;
+  let h = document.getElementById("h").value;
+  let i = document.getElementById("i").value;
+  document.getElementById("meme").innerHTML = "  Look, I guarantee therell be " + a + " times. I guarantee that at some  "+ b + " , "+ c +  " or both of us is gonna want to get out of this "+ d +" But I also guarantee that if I don`t ask you to be  "+ e +" I`ll "+ f + "  it for the rest of my "+g+ " , because I know, in my " +h+ " , you`re the  " +i+ "  one for me.  "+"  HIT REFRESH TO TRY AGAIN " ;
+
+
+}
+
